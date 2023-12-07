c5f85d2459e8c1883d6ab8e629d5c4defd6f8c2b
AnthonyDinunzioSWE <anthonydinunzioswe@gmail.com>
2023-12-06 20:37:27 -0700

    Docker Files Added

---
+++ integrity/build/precache-manifest.2a4077d9ffca1d5759efbe659699e595.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a172c0517eb30921539a51e6685ef71f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cabeaa24cc1765339487",
+    "url": "/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "8fe1caef11d4f3afc6a7",
+    "url": "/static/css/main.56863f57.chunk.css"
+  },
+  {
+    "revision": "cabeaa24cc1765339487",
+    "url": "/static/js/2.91ffafae.chunk.js"
+  },
+  {
+    "revision": "8fe1caef11d4f3afc6a7",
+    "url": "/static/js/main.6250581e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5c55035a36a0485443c8de0207564cf",
+    "url": "/static/media/compliance.d5c55035.png"
+  },
+  {
+    "revision": "350b14402ed2ee468ff3b1457e8bf9a7",
+    "url": "/static/media/development.350b1440.png"
+  },
+  {
+    "revision": "64f5a76656a002697290ba1ee9b05721",
+    "url": "/static/media/education.64f5a766.png"
+  },
+  {
+    "revision": "6a903e53f7e6bc52417120ebd17bdbf4",
+    "url": "/static/media/ep.6a903e53.png"
+  },
+  {
+    "revision": "4f57028b1f80b2f96d5c32b20c73e60b",
+    "url": "/static/media/logo.4f57028b.png"
+  },
+  {
+    "revision": "43e63447194d0584f09517705cf2a65e",
+    "url": "/static/media/risk.43e63447.png"
+  },
+  {
+    "revision": "f1837ada691b7c73486c38f192ad3a9c",
+    "url": "/static/media/security.f1837ada.png"
+  },
+  {
+    "revision": "12d02d55903f530e9f3e1326082e5f12",
+    "url": "/static/media/solutions.12d02d55.png"
+  },
+  {
+    "revision": "12847b74af6dc00818befc82ab353228",
+    "url": "/static/media/tech.12847b74.png"
+  }
+]);
--- integrity/build/service-worker.js
+++ integrity/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.912c45deff9ee293758e33c7c2bc9a50.js"
+  "/precache-manifest.2a4077d9ffca1d5759efbe659699e595.js"
 );
 
 self.addEventListener('message', (event) => {
